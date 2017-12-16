c0cc19e6849ffe6320e6201c434e6b1cfa9d39eb
Benjamin Sonley <benjamin.sonley@unic.com>
2017-12-16 01:38:35 +0100

    qunit tests are being run in puppeteer * needs cleaning up

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,22 +1,9 @@
-// import QUnit from 'qunit';
-// import factoryFontLoader from './index';
+(() => {
+  QUnit.module('Module A');
 
-// QUnit.test('a basic test example', (assert) => {
-//   var value = 'hello';
-//   assert.equal(value, 'hello', 'We expect value to be hell0');
-// });
-
-QUnit.test('ok test', assert => {
-  assert.ok(true, 'true succeeds');
-  assert.ok('non-empty', 'non-empty string succeeds');
-
-  assert.ok(false, 'false fails');
-  assert.ok(0, '0 fails');
-  assert.ok(NaN, 'NaN fails');
-  assert.ok('', 'empty string fails');
-  assert.ok(null, 'null fails');
-  assert.ok(undefined, 'undefined fails');
-});
-console.log('not executed');
+  QUnit.test('first test within module', assert => {
+    assert.ok(false, 'a dummy');
+  });
+})();
 
 // factoryFontLoader({ href: 'path/to/css' });
